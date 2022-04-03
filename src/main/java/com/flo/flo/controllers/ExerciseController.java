0fdc945644521da13988b784246b156bf604e14a
rus-alka <alla.rusetskaya@gmail.com>
2022-04-03 19:17:23 +0400

    Merge remote-tracking branch 'github/master'

--- src/main/java/com/flo/flo/controllers/ExerciseController.java
+++ src/main/java/com/flo/flo/controllers/ExerciseController.java
@@ -1,4 +1,9 @@
 package com.flo.flo.controllers;
 
+import com.flo.flo.repo.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ExerciseController {
+    @Autowired
+    private ProductRepository exerciseRepository;
 }
